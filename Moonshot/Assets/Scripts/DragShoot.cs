080a9c1260e209f02631af2a82d092c5a1500581
oars44 <54598471+oars44@users.noreply.github.com>
2020-11-25 03:17:46 -0800

    ball stopping/level restart

--- Moonshot/Assets/Scripts/DragShoot.cs
+++ Moonshot/Assets/Scripts/DragShoot.cs
@@ -1,30 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DragShoot : MonoBehaviour
 {
     public float power = 10f;
+    public GameObject deathMenu;
     public Vector2 minPower;
     public Vector2 maxPower;
+    
 
     private Rigidbody2D ball;
     private Camera cam;
     private Vector2 force;
     private Vector3 startPoint;
     private Vector3 endPoint;
+    private bool grounded = false;
+
+    private trajectory_render trail;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
         ball = GetComponent<Rigidbody2D>();
+        trail = GetComponent<trajectory_render>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        //Debug.Log(grounded);
+
+        if (Input.touchCount > 0 && grounded)
         {
             Touch touch = Input.GetTouch(0);
 
@@ -34,6 +43,13 @@ public class DragShoot : MonoBehaviour
                 startPoint.z = 15;
             }
 
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 current = cam.ScreenToWorldPoint(touch.position);
+                current.z = 15;
+                trail.Render(startPoint, current);
+            }
+
             if (touch.phase == TouchPhase.Ended)
             {
                 endPoint = cam.ScreenToWorldPoint(touch.position);
@@ -41,7 +57,35 @@ public class DragShoot : MonoBehaviour
 
                 force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x), Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
                 ball.AddForce(force * power, ForceMode2D.Impulse);
+                trail.EndLine();
+                grounded = false;
             }
         }
+    }
+
+   private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "planet")
+        {
+            grounded = true;
+            Vector2 vel = ball.velocity;
+
+            ball.AddForce(-(vel/2), ForceMode2D.Impulse);
+           
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Boundary")
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        deathMenu.SetActive(true);
+        Destroy(gameObject);
     }
 }
---
+++ Moonshot/Assets/Scripts/trajectory_render.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trajectory_render : MonoBehaviour
+{
+    public LineRenderer trail;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        trail = GetComponent<LineRenderer>();
+        trail.sortingLayerName = "Foreground";
+    }
+
+    public void Render(Vector3 startpoint, Vector3 endpoint)
+    {
+        trail.positionCount = 2;
+        Vector3[] points = new Vector3[2];
+        points[0] = startpoint;
+        points[1] = endpoint;
+
+        trail.SetPositions(points);
+    }
+
+    public void EndLine()
+    {
+        trail.positionCount = 0;
+    }
+}
